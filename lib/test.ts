52ccf961ea8894f77aa74e2ba42370eb6c270cf4
Bob Holt <bob.holt@splice.com>
2022-12-01 10:36:10 -0500

    update base project

---
+++ lib/test.ts
@@ -0,0 +1,8 @@
+export function assert(a: boolean): void {
+  if (a) {
+    console.log('ok');
+  } else {
+    throw new Error("fail");
+    ;
+  }
+}
---
+++ test/template.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env ts-node
+
+import { assert } from '../lib/test.js'
+
+assert(true);
