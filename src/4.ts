c4e03a2c37ade1131baaebf1ecafa1f125493c87
Bob Holt <bobholt@gmail.com>
2022-12-04 10:46:24 -0500

    refactor day 4

--- src/4.ts
+++ src/4.ts
@@ -32,6 +32,27 @@ type Assignment = RecordOf<AssignmentProps>;
 const makeAssignment: Record.Factory<AssignmentProps> =
   Record({ min: 0, max: 0 });
 
+function isFullOverlap(a1: Assignment, a2: Assignment): boolean {
+  return (a1.min <= a2.min && a1.max >= a2.max) ||
+    (a1.min >= a2.min && a1.max <= a2.max);
+}
+
+function isAnyOverlap(a1: Assignment, a2: Assignment): boolean {
+  return (a1.min <= a2.min && a1.max >= a2.min) ||
+    (a1.min <= a2.max && a1.max >= a2.max) ||
+    (a1.min >= a2.min && a1.max <= a2.max);
+}
+
+function testAssignments(data: FileData, test: Function): number {
+  return data.reduce((acc, curr) => {
+    const [a1, a2] = lineToAssignments(curr);
+    if (test(a1, a2)) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0)
+}
+
 function stringToAssignment(a: string): Assignment {
   const [min, max] = a.split('-');
   return makeAssignment({ min: Number(min), max: Number(max) })
@@ -42,37 +63,10 @@ function lineToAssignments(row: string): [Assignment, Assignment] {
   return [stringToAssignment(ensure(a1)), stringToAssignment(ensure(a2))];
 }
 
-function findFullOverlaps(data: FileData): number {
-  return data.reduce((acc, curr) => {
-    const [a1, a2] = lineToAssignments(curr);
-    if (
-      (a1.min <= a2.min && a1.max >= a2.max) ||
-      (a1.min >= a2.min && a1.max <= a2.max)
-    ) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0)
-}
-
-function findAnyOverlaps(data: FileData): number {
-  return data.reduce((acc, curr) => {
-    const [a1, a2] = lineToAssignments(curr);
-    if (
-      (a1.min <= a2.min && a1.max >= a2.min) ||
-      (a1.min <= a2.max && a1.max >= a2.max) ||
-      (a1.min >= a2.min && a1.max <= a2.max)
-    ) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0)
-}
-
 function program(data: FileData): Response {
   return [
-    findFullOverlaps(data),
-    findAnyOverlaps(data),
+    testAssignments(data, isFullOverlap),
+    testAssignments(data, isAnyOverlap),
   ];
 }
 
